4f60bad84f19b736190f5e786600e9ccce37da72
Sanches <autoscript@gmail.com>
2021-03-22 12:26:05 +0300

    000

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Generics_test_1
@@ -28,7 +29,17 @@ namespace Generics_test_1
                 }
             }
 
+            int[] numbers = { 0, 2, 4, 6, 8, 10 };
 
+            //using System.Collections;
+            IEnumerator ie = numbers.GetEnumerator(); // получаем IEnumerator
+            while (ie.MoveNext())   // пока не будет возвращено false
+            {
+                int item = (int)ie.Current;     // берем элемент на текущей позиции
+                Console.WriteLine(item);
+            }
+            ie.Reset(); // сбрасываем указатель в начало массива
+            Console.Read();
 
 
         }
